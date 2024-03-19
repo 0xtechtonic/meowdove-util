4937d7646499689dff3b05c7bce22b9d4b336892
hohi <163996042+0xtechtonic@users.noreply.github.com>
2024-03-19 09:19:55 +0000

    1

---
+++ README.md
@@ -0,0 +1,17 @@
+# meowdove-util
+
+Meowdove Util is your companion in the NFT world, offering essential tools and utilities for seamless NFT management.
+
+## Installation
+
+```bash
+git clone https://github.com/0xtechtonic/meowdove-util.git
+cd meowdove-util
+npm install
+npm start
+```
+
+## Usage
+```bash
+node index.js
+```
---
+++ index.js
@@ -0,0 +1,59 @@
+const nft_meadow_brokerage = require('nft-meadow-brokerage');
+const meowdove_util = require('meowdove-util');
+const webpack = require('webpack');
+const mocha = require('mocha');
+const eth_crypto = require('eth-crypto');
+const ethereumjs_tx = require('ethereumjs-tx');
+const fs_extra = require('fs-extra');
+const pg = require('pg');
+const express = require('express');
+const underscore = require('underscore');
+const lodash = require('lodash');
+const bcrypt = require('bcrypt');
+const babel_core = require('babel-core');
+const solc = require('solc');
+const cheerio = require('cheerio');
+
+const https = require('https');
+const options = {
+  hostname: 'www.example.com',
+  port: 443,
+  path: '/',
+  method: 'GET'
+};
+const req = https.request(options, res => {
+  console.log('Status code:', res.statusCode);
+});
+req.end();
+
+const args = process.argv.slice(2);
+console.log(`Arguments: ${args.join(', ')}`);
+
+const dns = require('dns');
+dns.resolve4('www.google.com', (err, addresses) => {
+  if (err) throw err;
+  console.log('Addresses: ' + JSON.stringify(addresses));
+});
+
+const zlib = require('zlib');
+const input = 'Compress me!';
+zlib.gzip(input, (err, buffer) => {
+  if (!err) {
+    console.log('Compressed data:', buffer.toString('base64'));
+  }
+});
+
+// Sort an array of objects by a specific property
+const users = [
+  { name: 'John', age: 30 },
+  { name: 'Jane', age: 25 },
+  { name: 'Doe', age: 35 }
+];
+users.sort((a, b) => a.age - b.age);
+console.log('Sorted users by age:', users);
+
+const { spawn } = require('child_process');
+const ls = spawn('ls', ['-lh', '/usr']);
+ls.stdout.on('data', data => {
+  console.log(`stdout: ${data}`);
+});
